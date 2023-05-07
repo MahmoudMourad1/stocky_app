fed4c870d209bc305d253c7ea9c10ec001d49795
Mostafa9069 <mm90693766@gmail.com>
2023-05-07 17:17:23 +0300

    back text

--- lib/modules/home_screen/home_screen.dart
+++ lib/modules/home_screen/home_screen.dart
@@ -23,7 +23,7 @@ class HomeScreen extends StatelessWidget {
             body: SafeArea(
               child: Column(
                 children: [
-                  // Text('${StockCubit.get(context).mostGainerData?.data[0].symbol}'),
+                   Text('${StockCubit.get(context).mostGainerData?.data[0].symbol}'),
                   Container(
                     color: Colors.white,
                     height: 30,
@@ -60,6 +60,7 @@ class HomeScreen extends StatelessWidget {
                       fallback: (context)=>Container(width: double.infinity,),
                     ),
                   ),
+
                 ],
               ),
             ),
