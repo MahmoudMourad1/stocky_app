25de7f82e93991e9c8bca467821b5957db482bfd
Mostafa9069 <mm90693766@gmail.com>
2023-05-18 14:13:41 +0300

    Ticker Screen last update

--- lib/modules/ticker_screen/ticker_screen.dart
+++ lib/modules/ticker_screen/ticker_screen.dart
@@ -2,9 +2,12 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:stock_twit/models/ticker_model.dart';
 
 import 'package:stock_twit/shared/cubit/cubit.dart';
 import 'package:stock_twit/shared/cubit/states.dart';
+// import 'package:syncfusion_flutter_charts/charts.dart';
+
 class TickerScreen extends StatelessWidget {
   const TickerScreen({
     super.key,
@@ -23,102 +26,86 @@ class TickerScreen extends StatelessWidget {
     return BlocProvider(
       create: (BuildContext context)=>StockCubit()..GetTickerData(from: from, to: to, symbol: symbol),
 
-      child: BlocConsumer<StockCubit,StockStates>(listener:(context,state){} ,builder:(context,state)=>Scaffold(
-
-        body: SafeArea(
-          child: Column(
-            children: [
-              ConditionalBuilder(condition: StockCubit.get(context).tickerData!=null,
-                  builder: (context)=>Container(
-                    width: 300,
-                    height: 270.0,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        // gradient: LinearGradient(
-                        //   colors: [Color(0xffba160a), Color(0x91ba160a)],
-                        //   stops: [0, 1],
-                        //   begin: Alignment.topLeft,
-                        //   end: Alignment.bottomRight,
-                        // )
-                        gradient: LinearGradient(
-                          colors: [Color(0xff3861FB).withOpacity(0.9), Color(0xff16b5fc).withOpacity(0.8)],
-                          stops: [0.3, 1],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        )
-
-
-
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            crossAxisAlignment:CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                padding: EdgeInsets.all(10.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  color: Colors.white.withOpacity(0.3),
-                                ),
-                                child:CachedNetworkImage(
-                                  imageUrl: "https://fmpcloud.io/image-stock/${StockCubit.get(context).tickerData!.symbol}.png",
-                                  placeholder: (context, url) => new CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) => new Icon(Icons.error),
-                                  height: 30,
-                                  width: 30,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Spacer(),
-                              Container(
-                                padding: EdgeInsets.all(10.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  color: Colors.white.withOpacity(0.4),
-                                ),
-                                child:Column(
-                                  children: [
-                                    Icon(StockCubit.get(context).tickerData!.data[0].change>0? Icons.show_chart:Icons.waterfall_chart_outlined,color: StockCubit.get(context).tickerData!.data[0].change>0 ? Colors.green:Colors.red,),
-                                    Text('${StockCubit.get(context).tickerData!.data[0].change}%',style: TextStyle(fontSize: 10.0,color: Colors.white),)
-                                  ],
-                                ),
-                              ),
-                            ],
+      child: BlocConsumer<StockCubit,StockStates>(listener:(context,state){} ,builder:(context,state){
+       TickerModel? ticker =StockCubit.get(context).tickerData;
+        return Scaffold(
+          body: SafeArea(
+            child: ConditionalBuilder(
+              condition: ticker!=null,
+              fallback: (context)=>Center(child: CircularProgressIndicator(),),
+              builder: (context)=>Column(
+                children: [
+                  //Instead of appbar
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50.0),
+                            color: Colors.grey.shade200.withOpacity(0.7),
                           ),
-                          Spacer(),
-                          Container(
-                            width: double.infinity,
-                            child: Column(
-                              children:   [
-                                Text('${StockCubit.get(context).tickerData!.data[0].volume}',style: TextStyle(color: Colors.white,fontSize: 20.0,fontWeight: FontWeight.w500),)],
-                            ),
-                          ),
-                          Spacer(),
-                          Row(
-                            children: [
-                              Text('${StockCubit.get(context).tickerData!.data[0].date} ',style: TextStyle(color:Colors.white,fontSize: 15.0),)
-                            ],
-                          ),
-
-                        ],
+                          child: IconButton(onPressed: (){
+                            Navigator.pop(context);
+                          }, icon: Icon(Icons.arrow_back)),
+                        ),
                       ),
-                    ),
-
-
+                      Spacer(),
+                      CachedNetworkImage(
+                        imageUrl: "https://fmpcloud.io/image-stock/${ticker!.symbol}.png",
+                        placeholder: (context, url) => new CircularProgressIndicator(),
+                        errorWidget: (context, url, error) => new Icon(Icons.error),
+                        color: ticker.symbol=='AAPL'? Colors.black.withOpacity(0.78):null,
+                        height: 80,
+                        width: 80,
+                        fit: BoxFit.cover,
+                      ),
+                      // SizedBox(width: 10,),
+                    Spacer(),
+                      // Text('${ticker.symbol}',style: TextStyle(fontSize: 35,fontWeight: FontWeight.w900,color: Colors.black ))
+                    ],
                   ),
-                  fallback: (context)=>CircularProgressIndicator())
-            ],
-          ),
-        ),
 
-      ),),
+                  //Drawing Chart
+
+                  // Container(
+                  //   child: SfCartesianChart(
+                  //     primaryXAxis: CategoryAxis(),
+                  //     series: <ChartSeries>[
+                  //       LineSeries<ChartData, String>(
+                  //           dataSource: [
+                  //             ChartData('Jan', 35, Colors.red),
+                  //             ChartData('Feb', 28, Colors.green),
+                  //             ChartData('Mar', 34, Colors.blue),
+                  //             ChartData('Apr', 32, Colors.pink),
+                  //             ChartData('May', 40, Colors.black)
+                  //           ],
+                  //           pointColorMapper:(ChartData data, _) => data.color,
+                  //           xValueMapper: (ChartData data, _) => data.x,
+                  //           yValueMapper: (ChartData data, _) => data.y)
+                  //     ],
+                  //   ),
+                  // ),
+
+                ],
+              ),
+            ),
+          ),
+
+        );
+      }),
 
 
 
     );
   }
+
 }
+// class ChartData {
+//   ChartData(this.x, this.y, this.color);
+//   final String x;
+//   final double y;
+//   final Color color;
+// }
